ee1abf1bbd34c4384f74ea8a13db0d28686dd550
Kenneth Truong <kenneth.e.truong@gmail.com>
2017-10-18 20:22:20 -0700

    Fix Email Script

--- api/actions/repo/scripts/email.sh
+++ api/actions/repo/scripts/email.sh
@@ -14,7 +14,8 @@ if [[ -z $3 ]]; then
   # Loop until all grades are sent
   while [ $counter -lt ${#grades[@]} ]; do
     user="${grades[${counter}]%.*}"
-    export MAILTO="$user@acsmail.ucsd.edu"
+    studentEmail="$user@acsmail.ucsd.edu"
+    export MAILTO="$studentEmail"
     export BCC="$1"
     export SUBJECT="$2"
     (
@@ -25,7 +26,7 @@ if [[ -z $3 ]]; then
        echo "Content-Type: text/html; charset=utf-8"
        echo "Content-Disposition: inline"
        cat ${grades[${counter}]}
-    ) | /usr/sbin/sendmail
+    ) | /usr/sbin/sendmail $studentEmail
     counter=$((counter+1))
   done
 fi
@@ -58,4 +59,4 @@ Content-Transfer-Encoding: $ENCODING
 EOF
 base64 $FILE
 echo ""
-echo "--$boundary" ) | /usr/sbin/sendmail
+echo "--$boundary" ) | /usr/sbin/sendmail $TO
